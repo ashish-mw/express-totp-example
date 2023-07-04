6c5b006cf682c3b8be69b4446f8bd8dccfefa803
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2023-07-04 19:20:52 +0530

    client timeout added

---
+++ README.md
@@ -0,0 +1,14 @@
+# TOTP based logins
+
+- Run the `client` and `server` separately
+- For both the start command is
+```
+$ npm run dev
+```
+- Have a tool like google authenticator ready!
+
+- Visit: http://localhost:5173/
+
+## Credits
+
+- https://github.com/wuyanxin/totp.js/tree/master
--- client/src/components/GetQRCode.jsx
+++ client/src/components/GetQRCode.jsx
@@ -6,20 +6,38 @@ import { getQrCode, verifyTOTP } from "../api";
 function GetQRCode({ token, handleCodeUpdate }) {
   const [url, setUrl] = useState("");
   const [code, setCode] = useState("");
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [kickOffTimer, setKickOffTimer] = useState(false);
+
   useEffect(() => {
     async function getQRUrl() {
       try {
         const res = await getQrCode(token);
         setUrl(res.data.url);
+        setKickOffTimer(true);
       } catch (error) {
         console.log(error);
       }
     }
 
-    if (token) {
+    if (token && timeLeft == 0) {
+      setTimeLeft(30);
       getQRUrl();
     }
-  }, [token]);
+  }, [token, timeLeft]);
+
+  useEffect(() => {
+    let interval = null;
+    if (kickOffTimer) {
+      interval = setInterval(() => {
+        setTimeLeft((prev) => prev - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [kickOffTimer]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,6 +66,9 @@ function GetQRCode({ token, handleCodeUpdate }) {
       {url && (
         <>
           <QRCode value={url} />
+
+          <p>Expires in {timeLeft} seconds</p>
+
           <form style={{ marginTop: "10px" }} onSubmit={handleSubmit}>
             <label htmlFor="code">Code</label>
             <input
